f8e296363c265b02cd5608544f8ecff057be3275
Sarah Morris <sarah.morris9@gmail.com>
2018-03-12 19:06:12 -0500

    working on starting call to different api

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 'use strict';
+const {get} = require('http')
+
+get('https://api.iextrading.com/1.0/stock/aapl/price', (res) => {
+    const statusCode = res.statusCode;
+    const contentType = res.headers['content-type'];
+})
 
 // method for apple stock
 // GET https://api.iextrading.com/1.0/stock/aapl/price
